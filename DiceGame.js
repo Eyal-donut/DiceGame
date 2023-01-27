ab7da538f2e9c7053d5d384fef2421a13af3b2ea
Eyal-donut <LilaEyal@gmail.com>
2023-01-28 00:10:54 +0100

    106

--- DiceGame.js
+++ DiceGame.js
@@ -123,12 +123,13 @@ const afterGameOver = () => {
     player1Heading.innerHTML = `<h6>PLAYER 1</h6>`;
     youWinContainerP1.append(youWin);
     youWinContainerP2.append(passedTargetScore);
+    toggleFinger();
   }
   if (player1.score === gameTarget) {
     winner = player1;
     player1InterfaceContainer.classList.add("winner");
     player1Heading.innerHTML = `<h6>PLAYER 1</h6>`;
-    youWinContainerP1.append(youWin);
+    youWinContainerP1.append(youWin); toggleFinger();
   }
 };
 
